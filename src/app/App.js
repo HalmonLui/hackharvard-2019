e3db4654392ccf72085c8645b48fc41f6842927d
Lui <luih@wit.edu>
2019-10-19 11:39:43 -0400

    Working camera module

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import CameraPage from "./camerapage";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <CameraPage />
+      </View>
+    );
+  }
+}
---
+++ src/app/camerapage.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, Text } from "react-native";
+import * as Permissions from "expo-permissions";
+import { Camera } from "expo-camera";
+
+import styles from "./styles";
+
+export default class CameraPage extends React.Component {
+  camera = null;
+
+  state = {
+    hasCameraPermission: null
+  };
+
+  async componentDidMount() {
+    const camera = await Permissions.askAsync(Permissions.CAMERA);
+    const audio = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    const hasCameraPermission =
+      camera.status === "granted" && audio.status === "granted";
+
+    this.setState({ hasCameraPermission });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>Access to camera has been denied.</Text>;
+    }
+
+    return (
+      <View>
+        <Camera style={styles.preview} ref={camera => (this.camera = camera)} />
+      </View>
+    );
+  }
+}
