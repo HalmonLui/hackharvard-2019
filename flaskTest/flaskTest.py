1d5acd304990c92bcbe66fa9b335d503d89e8263
Aaron Cheng <aaroncheng@umass.edu>
2019-10-19 13:58:19 -0400

    Changes made

--- flaskTest/flaskTest.py
+++ flaskTest/flaskTest.py
@@ -15,22 +15,42 @@ def hello():
 def getAnimeName(imageLink):
     r = requests.get(f'https://trace.moe/api/search?url={imageLink}')
     response = r.json()
-    print("GETTING THE ANIME BOIS")
     print(type(response))
     result = json.dumps(response)
     jsonResult = json.loads(result)
-    print("JFIOEJFIOAJFIOE")
     print(jsonResult['CacheHit'])
 
-    animeName = str(jsonResult['docs'][0]['anime'])
+    animeName = str(jsonResult['docs'][0]['title_english'])
     animeEpisode = str(jsonResult['docs'][0]['episode'])
-    animeSimilarity = str(jsonResult['docs'][0]['similarity'])
-    animeTimeMin = str(datetime.timedelta(seconds = jsonResult['docs'][0]['at']))
+    #animeSimilarity = str((jsonResult['docs'][0]['similarity']))
+    animeSimilarity = '{0:.1f}'.format((float(("%.3f" % round((jsonResult['docs'][0]['similarity']),4)))) * 100)
 
-    resultString = (f'Anime: {animeName} Ep: {animeEpisode} Similarity: {animeSimilarity} Time: {animeTimeMin}')
+    animeTimeMin = str(datetime.timedelta(seconds = jsonResult['docs'][0]['at']))
+    animeMALID = str(jsonResult['docs'][0]['mal_id'])
+
+    resultString = (f'Anime: {animeName} Ep: {animeEpisode} Similarity: {animeSimilarity}% Time: {animeTimeMin}')
+
+    animeDict = {
+    "anime": animeName,
+    "episode:": animeEpisode,
+    "confidence": animeSimilarity,
+    "time": animeTimeMin,
+    "malID": animeMALID
+    }
+
+@app.route("/anime/<path:animeMALID>")
+def getAnimeInfo(animeMALID):
+    r = requests.get(f'https://api.jikan.moe/v3/anime/{animeMALID}')
+    response = r.json()
+    print(type(response))
+    result = json.dumps(response)
+    jsonResult = json.loads(result)
+    print(jsonResult['url'])
+
+
+
+
 
 # printing result as string
-
-    most_common = collections.Counter(result).most_common(1)
-    print(most_common)
-    return(resultString)
+    print(type((float(("%.3f" % round((jsonResult['docs'][0]['similarity']),2))))))
+    return(animeDict)
