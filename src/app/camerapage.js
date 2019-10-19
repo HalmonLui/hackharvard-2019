737a9af34fe1982995784e91db2c4c8c1c08f3a1
Lui <luih@wit.edu>
2019-10-19 15:23:12 -0400

    Created working imgur upload

--- src/app/camerapage.js
+++ src/app/camerapage.js
@@ -33,11 +33,36 @@ export default class CameraPage extends React.Component {
       let photo = await this.camera.takePictureAsync(
         (options = { quality: 0.9, base64: true })
       );
+
       this.setState({ status: "Saving photo...", photo: photo });
 
-      console.log(photo.base64);
+      //console.log(photo.base64);
+      let formData = new FormData();
+      formData.append("image", photo.base64);
+      formData.append("type", "base64");
+
+      const response = await fetch("https://api.imgur.com/3/image", {
+        method: "POST",
+        headers: {
+          Authorization: "Client-ID 975dcffbf8a2e52" // add your Imgur App ID here
+        },
+        body: formData
+      });
+
+      let response_body = await response.json(); // get the response body
+      console.log("GOT StuFF");
+      console.log(response_body.data.link);
+      this.getAnime(response_body.data.link);
     }
   };
+
+  async getAnime(link) {
+    const response = await fetch("http://putesco.serveo.net/anime/" + link, {
+      method: "GET"
+    });
+    let response_body = await response.json(); //get response body
+    console.log(response_body);
+  }
 
   render() {
     const { hasCameraPermission } = this.state;
